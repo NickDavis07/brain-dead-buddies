51639d296439074b8e31c6921b9b1c6675b5c9f5
unknown <jamesgarlandraney@gmail.com>
2025-04-01 15:51:36 -0600

    Updated package.json and vite.config.ts and pages and components

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(), // Add Tailwind CSS plugin here
+    react(), // Add React plugin here
   ],
   server: {
     port: 3000,
