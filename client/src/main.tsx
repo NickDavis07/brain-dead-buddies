a2b1e402b1f2705cb732e7099b64d99582995da7
Nick Davis <nickwagner@live.com>
2025-04-01 19:17:22 -0600

    home.tsx changes, commented previous code to get the application functioning

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-
 import App from './App.jsx';
 import Home from './pages/Home';
 import Checklist from './pages/Checklist';
