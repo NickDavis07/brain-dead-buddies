4fe25f3f3afcbb992f33234b1448bab0527470e5
Nick Davis <nickwagner@live.com>
2025-04-09 20:14:26 -0600

    added mergedResolvers

--- server/src/schemas/index.ts
+++ server/src/schemas/index.ts
@@ -1,4 +1,13 @@
 import typeDefs from './typeDefs.js';
 import resolvers from './resolver.js';
 
-export { typeDefs, resolvers };
+const mergedResolvers = {
+    Query: {
+      ...resolvers.Query,
+    },
+    Mutation: {
+      ...resolvers.Mutation,
+    },
+  };
+
+export { typeDefs, mergedResolvers as resolvers };
